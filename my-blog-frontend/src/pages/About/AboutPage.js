feaa9354c157a4cd3047a036517dc3c2adcb3b4a
AnchuK <anchukariyapath@gmail.com>
2022-02-11 10:14:42 +0530

    changes

--- my-blog-frontend/src/pages/About/AboutPage.js
+++ my-blog-frontend/src/pages/About/AboutPage.js
@@ -23,12 +23,11 @@ function AboutPage(props) {
                     I am so ready to have a place to share all my travels on, and share my life with you!
                 </p>
                 <p>
-                    I’m Shobith Chandran K, from India. But I’ve lived in Budapest, Germany, and the US before! I don’t know,
+                    I’m Anchu K, from India. But I’ve lived in Budapest, Germany, and the US before! I don’t know,
                     I just never felt like “home” in a specific country.
                     Is that just me or do you also never really know the answer to the question where you’re from?
                     So like I have a Swiss passport, but lived at the German border, 
-                    so I don’t really speak Swiss German, but my parents are from India, 
-                    so I grew up Indian. Oh, and I’ve also lived abroad in the States.
+                   
                 </p>
                 <p>
                     What is Your Story? I would love to hear.
