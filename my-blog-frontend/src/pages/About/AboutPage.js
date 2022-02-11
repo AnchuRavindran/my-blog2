06a865e3574c87a1b4bea65221b87cec20fa4176
AnchuK <anchukariyapath@gmail.com>
2022-02-11 12:28:59 +0530

    changes

--- my-blog-frontend/src/pages/About/AboutPage.js
+++ my-blog-frontend/src/pages/About/AboutPage.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './AboutPage.css'
-import me from './me.png'
+import me from './me.jpg'
 import { Link } from 'react-router-dom';
 
 
 function AboutPage(props) {
-    // const url1 = "https://github.com/ShobithChandran";
-    // const url2 = "https://www.linkedin.com/in/shobith-chandran-k-63a072199/";
+    const url1 = "";
+    const url2 = "";
     return(
         <div >
             <br /><br /><br /><br />
             <h1 id='title'> About Me </h1>
             <br />
             <h1 id='name'>Aanchu K </h1>
-            <div className="image-container">
+            {/* <div className="image-container">
                 <img src={me} alt="me" height="390" width="350" />
-            </div>
+            </div> */}
             <div id='abtdisc'>
                 <center>
                 <p>
